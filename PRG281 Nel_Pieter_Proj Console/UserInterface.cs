4a95e4c357ce9f54ca1d0a946bfecebb288d48d0
Vinkkamph <nelpwaruan@gmail.com>
2023-08-29 12:29:11 +0200

    Added fail conditions to SetLoanDetails()

--- PRG281 Nel_Pieter_Proj Console/UserInterface.cs
+++ PRG281 Nel_Pieter_Proj Console/UserInterface.cs
@@ -36,7 +36,51 @@ namespace PRG281_Nel_Pieter_Proj_Console
 
             loanType = (LoanType)loanTypeInt;
         }
+   
+        public void SetLoanDetails(Loan loan)
+        {
 
+            Console.WriteLine("Enter Loan Number: ");
+            string loanNumberRaw = Console.ReadLine();
+
+            Console.WriteLine("Enter Customer Name: ");
+            string customerName = Console.ReadLine();
+
+            Console.WriteLine("Enter Customer Surname: ");
+            string customerSurname = Console.ReadLine();
+
+            Console.WriteLine("Enter Loan Amount: ");
+            string loanAmountRaw = Console.ReadLine();
+
+            Console.WriteLine("Select Loan Term: ");
+            Console.WriteLine("0. Short (1 year)");
+            Console.WriteLine("1. Medium (3 years)");
+            Console.WriteLine("2. Long (5 years)");
+            string loanTermRaw = Console.ReadLine();
+
+
+            if(!int.TryParse(loanNumberRaw, out int loanNumber))
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid loan number");
+                SetLoanDetails(loan);
+            }
+
+            if (!double.TryParse(loanAmountRaw, out double loanAmount))
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid loan amount");
+                SetLoanDetails(loan);
+            }
+
+            if(!int.TryParse(loanTermRaw, out int loanTermInt) || loanTermInt > 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid loan term");
+                SetLoanDetails(loan);
+            }
+
+        }
 
     }
 }
